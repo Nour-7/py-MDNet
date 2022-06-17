a39f5c5174292847a5eb97d0f40fbc89ef71aa2e
Nour <Nour7011@gmail>
2022-06-17 23:15:31 +0200

    Fix error in Ground Truth box when running tracker

--- tracking/run_tracker.py
+++ tracking/run_tracker.py
@@ -105,12 +105,15 @@ def train(model, criterion, optimizer, pos_feats, neg_feats, maxiter, in_layer='
 def run_mdnet(img_list, init_bbox, gt=None, savefig_dir='', display=False):
 
     # Init bbox
-    if init_bbox.shape[1] == 8:
-        x_min = np.min(init_bbox[:, [0, 2, 4, 6]], axis=1)[:, None]
-        y_min = np.min(init_bbox[:, [1, 3, 5, 7]], axis=1)[:, None]
-        x_max = np.max(init_bbox[:, [0, 2, 4, 6]], axis=1)[:, None]
-        y_max = np.max(init_bbox[:, [1, 3, 5, 7]], axis=1)[:, None]
-        init_bbox = np.concatenate((x_min, y_min, x_max - x_min, y_max - y_min), axis=1)
+    # print(init_bbox)
+    # print(init_bbox[[0, 2, 4, 6]])
+    print(init_bbox.shape[0])
+    if init_bbox.shape[0] == 8:
+        x_min = np.min(init_bbox[[0, 2, 4, 6]])
+        y_min = np.min(init_bbox[[1, 3, 5, 7]])
+        x_max = np.max(init_bbox[[0, 2, 4, 6]])
+        y_max = np.max(init_bbox[[1, 3, 5, 7]])
+        init_bbox = (x_min, y_min, x_max - x_min, y_max - y_min)
     target_bbox = np.array(init_bbox)
     result = np.zeros((len(img_list), 4))
     result_bb = np.zeros((len(img_list), 4))
@@ -196,6 +199,7 @@ def run_mdnet(img_list, init_bbox, gt=None, savefig_dir='', display=False):
                 y_min = np.min(gt[:, [1, 3, 5, 7]], axis=1)[:, None]
                 x_max = np.max(gt[:, [0, 2, 4, 6]], axis=1)[:, None]
                 y_max = np.max(gt[:, [1, 3, 5, 7]], axis=1)[:, None]
+                gt = np.concatenate((x_min, y_min, x_max - x_min, y_max - y_min), axis=1)
             gt_rect = plt.Rectangle(tuple(gt[0, :2]), gt[0, 2], gt[0, 3],
                                     linewidth=3, edgecolor="#00ff00", zorder=1, fill=False)
             ax.add_patch(gt_rect)
@@ -286,6 +290,12 @@ def run_mdnet(img_list, init_bbox, gt=None, savefig_dir='', display=False):
             im.set_data(image)
 
             if gt is not None:
+                if gt.shape[1] == 8:
+                  print("ground")
+                  x_min = np.min(gt[:, [0, 2, 4, 6]], axis=1)[:, None]
+                  y_min = np.min(gt[:, [1, 3, 5, 7]], axis=1)[:, None]
+                  x_max = np.max(gt[:, [0, 2, 4, 6]], axis=1)[:, None]
+                  y_max = np.max(gt[:, [1, 3, 5, 7]], axis=1)[:, None]
                 gt_rect.set_xy(gt[i, :2])
                 gt_rect.set_width(gt[i, 2])
                 gt_rect.set_height(gt[i, 3])
